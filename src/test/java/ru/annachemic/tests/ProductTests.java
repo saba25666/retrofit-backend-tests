e557f70451372c6dedf22a0374e67c6b47131733
saba25666 <Anzhelika1991>
2021-10-21 19:59:40 +0300

    HW-6

--- src/test/java/ru/annachemic/tests/ProductTests.java
+++ src/test/java/ru/annachemic/tests/ProductTests.java
@@ -1,22 +1,29 @@
 package ru.annachemic.tests;
-
 import com.github.javafaker.Faker;
+import okhttp3.ResponseBody;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import ru.annachemic.db.dao.ProductsMapper;
 import ru.annachemic.dto.Product;
 import ru.annachemic.enums.CategoryType;
 import ru.annachemic.dto.Category;
 import ru.annachemic.service.CategoryService;
 import ru.annachemic.service.ProductService;
+import ru.annachemic.utils.DbUtils;
 import ru.annachemic.utils.RetrofitUtils;
 import java.io.IOException;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class ProductTests {
+    int productId;
+   static ProductsMapper productsMapper;
     static Retrofit client;
     static ProductService productService;
     static CategoryService categoryService;
@@ -28,6 +35,7 @@ public class ProductTests {
         client = RetrofitUtils.getRetrofit();
         productService = client.create(ProductService.class);
         categoryService = client.create(CategoryService.class);
+        productsMapper = DbUtils.getProductsMapper();
     }
 
     @BeforeEach
@@ -40,10 +48,14 @@ public class ProductTests {
 
     @Test
     void postProductTest() throws IOException {
+        DbUtils.countProducts (productsMapper);
         Response<Product> response = productService.createProduct(product).execute();
+        DbUtils.countProducts (productsMapper);
+        assert response.body ( ) != null;
         assertThat(response.body().getTitle(), equalTo(product.getTitle()));
         assertThat(response.body().getPrice(), equalTo(product.getPrice()));
         assertThat(response.body().getCategoryTitle(), equalTo(product.getCategoryTitle()));
+            productId = response.body().getId();
     }
 
     @Test
@@ -52,8 +64,14 @@ public class ProductTests {
         Response<Category> response = categoryService
                 .getCategory(id)
                 .execute();
+        assert response.body ( ) != null;
         assertThat(response.body().getTitle(), equalTo(CategoryType.FOOD.getTitle()));
         assertThat(response.body().getId(), equalTo(id));
     }
 
+    @AfterEach
+    void tearDown() throws IOException {
+        Response<ResponseBody> response = productService.deleteProduct(productId).execute();
+        MatcherAssert.assertThat (response.isSuccessful ( ), is (true));
+    }
 }
